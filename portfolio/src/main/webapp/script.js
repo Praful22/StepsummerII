d694b274b272785b7f2354b0d4ddcb38d861f3c0
Praful22 <prafulsigdel5@gmail.com>
2020-06-16 14:01:46 +0000

    Edit script.js file.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -38,6 +38,6 @@ function getCommentsUsingArrowFunctions() {
 /** Creates a map and adds it to the page. */
 function createMap() {
   const map = new google.maps.Map(
-      document.getElementById('map'),
+      document.querySelector('#map'),
       {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 }
