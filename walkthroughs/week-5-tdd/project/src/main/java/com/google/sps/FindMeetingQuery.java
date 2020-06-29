ea7a5747f484e750ac280e4717d4842964a0a7bb
Praful22 <prafulsigdel5@gmail.com>
2020-06-29 15:18:21 +0000

    Method to retrieve meeting time with no conflicts.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,66 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    long workingPeriod = request.getDuration();
+    int totalHours = 24*60;
+    // If meeting duration is greater than a day, then meeting cannot happen.
+    if (workingPeriod > TimeRange.WHOLE_DAY.duration()){ return new ArrayList<TimeRange>();}
+
+    // If no attendees are present, then the meeting can happen at any time of the day.
+    if (events.isEmpty()) return Arrays.asList(TimeRange.WHOLE_DAY);
+
+    ArrayList<TimeRange> attendeesWorkTime = getWorkTime(getTimeConflicts(events, request), workingPeriod);
+    
+    attendeesWorkTime;
   }
+  
+  private ArrayList<TimeRange> getWorkTime(List<TimeRange> timeConflicts, long workingPeriod) {
+    ArrayList<TimeRange> workTime = new ArrayList<>();
+    Collections.sort(timeConflicts, TimeRange.ORDER_BY_START);
+    int workTimeStart = TimeRange.START_OF_DAY;
+    int workTimeEnd = 0;
+    int trackMergingConflict = 0;
+    for (TimeRange timeConflict: timeConflicts){
+        workTimeEnd = timeConflict.start();
+        if (workingPeriod <= workTimeEnd - workTimeStart){
+          workTime.add(TimeRange.fromStartEnd(workTimeStart, workTimeEnd, false));
+        }
+        if (trackMergingConflict < timeConflict.end()) {
+          trackMergingConflict = timeConflict.end(); 
+        }
+        if (trackMergingConflict > timeConflict.end()) {
+          workTimeStart = trackMergingConflict;
+        }
+        else{
+          workTimeStart = timeConflict.end();
+        }
+    } 
+    if (TimeRange.END_OF_DAY - workTimeStart >= workingPeriod){
+        workTime.add(TimeRange.fromStartEnd(workTimeStart, TimeRange.END_OF_DAY, true));
+    }
+    return workTime;
+    }
+
+  private List<TimeRange> getTimeConflicts(Collection<Event> events, MeetingRequest request) {
+    List <TimeRange> timeConflicts = new ArrayList<>();
+    for (Event event: events) {
+      Set<String> eventAttendees = event.getAttendees();
+      if (!Collections.disjoint(eventAttendees, request.getAttendees()))
+        {
+        timeConflicts.add(event.getWhen());
+        }
+      }
+    return timeConflicts;
+    }
 }
